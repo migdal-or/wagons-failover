680e5c5e83ed23933575431a22b435a38ca428cf
Dmitry <dmitry.zvorikin@gmail.com>
2018-10-24 09:59:27 +0300

    v5

--- failover-p2.sh
+++ failover-p2.sh
@@ -7,26 +7,96 @@
 #iptables -t filter -A INPUT -i eth0 -j DROP
 #iptables -t filter -A FORWARD -i eth0 -p tcp -m tcp --dport 50000 -m state --state NEW -j ACCEPT
 
+# не включён ли уже маскарад? если включён, то похоже что наш скрипт уже гоняли. тогда нафиг пожалуйста.
+RULE_NUMBER=$(iptables -t nat -L --line-numbers | grep -m1 MASQUERADE | sed -rn 's/([0-9]*).*MASQUERADE.*/\1/p')
+if [ ! -v ${RULE_NUMBER:-} ]
+then
+  printf "Masquerade is already on. Please disable it and rerun because the script switches it.\n"
+#  exit 1
+fi
+
 # Set defaults if not provided by environment
 CHECK_DELAY=${CHECK_DELAY:-5}
 CHECK_IP=${CHECK_IP:-8.8.8.8}
 
 # Первый — 3G/LTE модем.
 PRIMARY_IF=${PRIMARY_IF:-br0.10}
-# проверить есть ли там вообще адрес
-PRIMARY_IP=$(ip address show dev "$PRIMARY_IF" | grep -m1 inet | sed -rn 's/.*inet ([^ ]*)\/.*$/\1/p')
-printf "ip is '$PRIMARY_IP'\n"
-
+# проверять есть ли там вообще адрес
 PRIMARY_GW=${PRIMARY_GW:-192.168.0.1}
+iptables -t nat -A POSTROUTING -o "$PRIMARY_IF" -j MASQUERADE
 
-RULE_NUMBER=$(iptables -t nat -L --line-numbers | grep -m1 MASQUERADE | sed -rn 's/([0-9]*).*MASQUERADE.*/\1/p')
-printf "rule is '$RULE_NUMBER'\n"
-if [ ! -v ${RULE_NUMBER:-} ]
-then
-	printf "Masquerade already on. Please disable it and rerun because the script switches it.\n"
-	exit 1
-else
-	printf "sfdsdfsd \n"
-fi
+# Второй — хартинги
+SECONDARY_IF=${SECONDARY_IF:-br0.20}
+# узнать айпи на хартинговом интерфейсе
+SECONDARY_NET=$(ip address show dev "$SECONDARY_IF" | grep -m1 inet | sed -rn 's/.*inet ([^ ]*).*$/\1/p')
+SECONDARY_IP=$(ip address show dev "$SECONDARY_IF" | grep -m1 inet | sed -rn 's/.*inet ([^ ]*)\/.*$/\1/p')
 
-printf "next\n"
+# Третий — ADSL-модем Zyxel
+TERTIARY_IF=${TERTIARY_IF:-br0.30}
+# узнать айпи на adsl интерфейсе
+TERTIARY_NET=$(ip address show dev "$TERTIARY_IF" | grep -m1 inet | sed -rn 's/.*inet ([^ ]*).*$/\1/p')
+TERTIARY_IP=$(ip address show dev "$TERTIARY_IF" | grep -m1 inet | sed -rn 's/.*inet ([^ ]*)\/.*$/\1/p')
+
+# Compare arg with current default gateway interface for route to healthcheck IP
+gateway_if() {
+  [[ "$1" = "$(ip r g "$CHECK_IP" | sed -rn 's/^.*dev ([^ ]*).*$/\1/p')" ]]
+}
+
+read
+# Cycle healthcheck continuously with specified delay
+while sleep "$CHECK_DELAY"
+# TODO проверять br0.10 только если там есть адрес
+# если адреса нету, значит модем выключен или сдох
+# однако модем может в любой момент включиться и поднять адрес, что приведёт к съезду всей таблицы
+# обработать это
+
+do
+  PRIMARY_IP=$(ip address show dev "$PRIMARY_IF" | grep -m1 inet | sed -rn 's/.*inet ([^ ]*)\/.*$/\1/p')
+  # if the primary interface is up
+  if [ -v ${PRIMARY_IP:-} ]
+  then
+    # primary interface is down
+    printf "sdss"
+  else
+    # адрес на модеме не пустой
+    if ping -I "$PRIMARY_IF" -c1 "$CHECK_IP" &>/dev/null
+    # If healthcheck succeeds from primary interface
+    then
+      # Are we using any of the backups?
+      if (gateway_if "$SECONDARY_IF") || (gateway_if "$TERTIARY_IF")
+      then # Switch to primary
+        echo ip r d default
+        echo ip r a default via "$PRIMARY_GW" dev "$PRIMARY_IF"
+        RULE_NUMBER=$(iptables -t nat -L --line-numbers | grep -m1 MASQUERADE | sed -rn 's/([0-9]*).*MASQUERADE.*/\1/p')
+        echo iptables -t nat -D POSTROUTING "$RULE_NUMBER"
+        echo iptables -t nat -A POSTROUTING -o "$PRIMARY_IF" -j MASQUERADE
+        # TODO: share my internet
+read
+      fi
+    else # гугл недоступен по основному интерфейсу
+      # Are we using the primary?
+      if gateway_if "$PRIMARY_IF"
+      then # Switch to backup
+        (nmap -sn "$SECONDARY_NET" --exclude "$SECONDARY_IP" -oG - | grep "Status: Up" | sed -rn 's/Host: ([^ ]*) \(.*/\1/p') | while read -r line
+        do
+          # в переменной у нас перебираются возможные гейтвеи. Если хоть один сработал, будем использовать его
+          echo $line
+          # надо проверить, есть ли в нём интернет
+          echo ip r d "$SECONDARY_NET"
+          echo ip r a $line dev "$SECONDARY_IF"
+          if ping -c1 -I "$SECONDARY_IF" "$CHECK_IP" &>/dev/null
+          then
+            # works, switch to this
+            echo ip r d default
+            echo ip r a default via "$line" dev "$SECONDARY_IF"
+                 # и раздать через адсл
+          fi
+        done
+  
+        echo ip r d default
+        # у нас неизвестен гейтвей на третьем
+        echo ip r a default via "$SECONDARY_GW" dev "$SECONDARY_IF"
+      fi
+    fi
+  fi
+done

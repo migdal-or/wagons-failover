2e3ede8846ac3612b5c6c9d81b3cba8216fb60c7
Dmitry Zvorykin <dmitry.zvorikin@gmail.com>
2018-11-01 16:21:24 +0300

    modified paths

--- check-internet-script-starter.sh
+++ check-internet-script-starter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen -L /home/user/internet-checkup.log -d -m -S inetcheck /home/user/wagons-failover/check-internet-script.sh
+screen -L /tmp/internet-checkup.log -d -m -S inetcheck /tmp/inetcheck/wagons-failover/check-internet-script.sh

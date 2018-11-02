74c74866dc3dbcc1b4f029049aa4318f172c2b29
Dmitry Zvorykin <dmitry.zvorikin@gmail.com>
2018-11-02 04:45:35 +0300

    changed paths, added our ip lookback and minor edits

--- check-internet-script.sh
+++ check-internet-script.sh
@@ -4,9 +4,13 @@ printf "this is the internet checkup script on 765/630"
 
 while true
 do
-	ping -c5 -W5 8.8.4.4 &
-	ping -c5 -W5 1.1.1.1 &
 	printf "\n\n"
-	date +"--- this is the check at %s %T ---"
+	date +"--- this is the check at %s %T %Z %z---"
+	printf "our ip is "
+	curl -s http://whatismyip.akamai.com/
+	printf " or maybe "
+	dig +short myip.opendns.com @resolver1.opendns.com
+        ping -c5 -W5 8.8.4.4 &
+        ping -c5 -W5 1.1.1.1 &
 	sleep 15
 done

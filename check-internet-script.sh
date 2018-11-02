ee84be028b96e46b912c11b84d8428f456bce570
Dmitry Zvorykin <dmitry.zvorikin@gmail.com>
2018-11-02 08:38:35 +0300

    added timeout to curl

--- check-internet-script.sh
+++ check-internet-script.sh
@@ -7,7 +7,7 @@ do
 	printf "\n\n"
 	date +"--- this is the check at %s %T %Z %z---"
 	printf "our ip is "
-	curl -s http://whatismyip.akamai.com/
+	curl -m 2 -s http://whatismyip.akamai.com/
 	printf " or maybe "
 	dig +short myip.opendns.com @resolver1.opendns.com
         ping -c5 -W5 8.8.4.4 &
